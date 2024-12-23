b0c1c96269e5cc50bd7b3fd7eff41c448cbf4dbf
Shikhar1808 <saxena.shikhar1808@gmail.com>
2024-12-23 19:55:29 +0530

    idk3

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import {defineConfig} from "vite";
  import react from '@vitejs/plugin-react'
 export default defineConfig({
-    base:"./",
-    build:{
-        minify:"terser",
-    },
+    // base:"./",
+    // build:{
+    //     minify:"terser",
+    // },
     plugins:[react()]
 })
