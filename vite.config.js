983af75bbff657c6cb45fe602ec37194bff254e0
Shikhar1808 <saxena.shikhar1808@gmail.com>
2024-12-23 20:19:21 +0530

    idk7

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,19 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     base: "/",
-    plugins:[react()],
+    plugins: [react()],
     build: {
         outDir: 'dist',
-        assetsDir: 'assets',
         rollupOptions: {
-            output: {
-                manualChunks: undefined
-            },
-            input:{
+            input: {
                 main: 'index.html',
-                game: 'game.html',
+                game: 'game.html'
             }
+        }
+    },
+    server: {
+        fs: {
+            strict: false
         }
     }
 })
